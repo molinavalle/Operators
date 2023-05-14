dfad0ffefa05fc8b3e4a0af40c3b3dbab8a1f50f
valle <valle@ALMOVA>
2023-05-14 16:46:57 +0200

    Add project files.

---
+++ Operators/Program.cs
@@ -0,0 +1,19 @@
+﻿//Math
+int n1 = 1;
+double n2 = 2.87; 
+float n3 = 3;
+decimal n4 = 3.678m;
+//increment (++) adds 1
+//decrement (--) substracts 1
+
+//assignment
+//=, += ,-=
+//equal (==)
+//not equal (!=)
+int num1 = 1;
+int num2 = 3;
+num1 += num2; //num1 = num1 + num2 = 4
+Console.WriteLine(num1);
+//relational
+//logical
+Console.ReadLine();
